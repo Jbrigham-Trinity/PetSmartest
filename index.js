fbf1931e8512c4cb66a41288edbd651a33206e63
Jeremy Brigham <jeremybrigham@Jeremys-MacBook-Air.local>
2024-03-03 15:35:05 -0600

    installed homepage through it

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//running this makes us able to load express files as web pages
+app.set('view engine', 'ejs')
+
+//check it out its our homepage
+
 app.get("/", function (req, res) {
+    /*
     let name = (typeof req.query.name === 'undefined') ? "World" : req.query.name;
     res.send(`Hello ${name}!`);
+    */
+    res.render('home')
 });
 
 app.get("/accounts", function (req, res) {
